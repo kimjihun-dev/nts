214495f403d0a1a4d54d33f3604298b200ec80ee
kimjihun-dev <kimjihun.dev@gmail.com>
2020-08-24 11:02:50 +0900

    commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    // bxslider
+    $('.slider').bxSlider({
+        mode: 'horizontal',
+        speed: 500, 
+        auto: true, 
+        mode:'fade', 
+        autoControls: false,
+        controls: false,
+    });
+});
